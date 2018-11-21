0cb81f8ce95f7567317a867cbbbed02e50600f2e
Joshua Ng <31525854+nessarus@users.noreply.github.com>
2018-11-21 23:48:59 +0800

    first draft

---
+++ project2-myshell/execute.c
@@ -0,0 +1,73 @@
+#include "myshell.h"
+
+/*
+   CITS2002 Project 2 2017
+   Name(s):		student-name1 (, student-name2)
+   Student number(s):	student-number-1 (, student-number-2)
+   Date:		date-of-submission
+ */
+
+// -------------------------------------------------------------------
+
+//  THIS FUNCTION SHOULD TRAVERSE THE COMMAND-TREE and EXECUTE THE COMMANDS
+//  THAT IT HOLDS, RETURNING THE APPROPRIATE EXIT-STATUS.
+//  READ print_shellcmd0() IN globals.c TO SEE HOW TO TRAVERSE THE COMMAND-TREE
+
+
+void find_path_execute(SHELLCMD *t)
+{
+    char *temp=strtok(PATH,":");
+    while(temp)
+    {
+        char *temp_path;
+        temp_path=strdup(temp);
+        strcat(temp_path,"/");
+        strcat(temp_path,t->argv[0]);
+        printf("temp_path is %s\n",temp_path);
+        FILE *fp=fopen(temp_path,"r");
+        if(fp!=NULL)
+        {
+            execv(temp_path,t->argv);
+        }
+        temp = strtok(NULL,":");
+        
+    }
+    return ;
+}
+
+
+int execute_shellcmd(SHELLCMD *t)
+{
+    int  exitstatus;
+
+    if(t == NULL) {			// hmmmm, that's a problem
+	exitstatus	= EXIT_FAILURE;
+    }
+    else {				// normal, exit commands
+	exitstatus	= EXIT_SUCCESS;
+    }
+    
+    pid_t fpid;
+    fpid=fork();
+    
+    if(fpid==0)// child process
+    {
+        char * strtemp;
+        strtemp=strchr(t->argv[0],'/');
+        if(strtemp!=NULL)
+        {
+           execv(t->argv[0],t->argv);
+        }
+
+        find_path_execute(t);
+        exit(EXIT_FAILURE);
+    }
+    else // parent process
+    {
+        wait(&fpid);
+    }
+    
+    
+
+    return exitstatus;
+}
